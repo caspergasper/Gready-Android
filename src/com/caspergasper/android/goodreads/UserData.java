70074b437492366e3b6d1feca18bb339f4ff7ddd
Casper Gasper <casper.gasper@gmail.com>
2010-06-26 16:24:30 +0100

    Fix for Issue 5 (invalid UTF-8 char breaks output)

--- src/com/caspergasper/android/goodreads/UserData.java
+++ src/com/caspergasper/android/goodreads/UserData.java
@@ -3,12 +3,15 @@ package com.caspergasper.android.goodreads;
 import static com.caspergasper.android.goodreads.GoodReadsApp.TAG;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import android.util.Log;
@@ -45,13 +48,23 @@ class UserData {
 	}
 	
 	void getSAXUpdates(InputStream is) {
+		Reader reader;
+		InputSource source;
 		try {
 			Log.d(TAG, "Start parsing.");
-			parser.parse(is, handler);
+			// Have to strip off non-UTF-8 characters  -
+			// sadly there's a few of those in the XML :-( 
+			reader = new InputStreamReader(is, "UTF-8");
+			source = new InputSource(reader);
+			parser.parse(source, handler);				
 			Log.d(TAG, "End parsing.");
+			
 		} catch (SAXException e) {
-			// XML not well-formed -- ignore.
-			Log.e(TAG, "SAXException: " + e.toString());	
+			// XML not well-formed -- abort but carry on reading next XML file.
+			Log.e(TAG, "SAXException in getSAXUpdates: " + e.toString());
+			if(books.size() != 0) {
+				Log.e(TAG, books.get(books.size() - 1).title);
+			}
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
@@ -70,9 +83,5 @@ class UserData {
 		}
             return;
 	}
-
-	
-
-
 	
 }
